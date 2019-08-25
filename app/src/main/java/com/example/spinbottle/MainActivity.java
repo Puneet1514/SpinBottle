4ca05aa40e794e4513af93ab86c2fe6231d9bd6e
puneet1514 <puneetverma951761@gmail.com>
2019-08-25 12:12:59 -0400

    Click To Spin

---
+++ app/src/main/java/com/example/spinbottle/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.spinbottle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Animatable;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Random random = new Random();
+    ImageView bottle;
+    boolean spinning = false;
+    int lastde = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottle = (ImageView)findViewById(R.id.bottle);
+    }
+
+    void rotate(View v)
+    {
+        if ( !spinning ) {
+            int rand = random.nextInt() / 10000;
+            int pivotx = bottle.getWidth() / 2;
+            int pivoty = bottle.getHeight() / 2;
+            Animation rot = new RotateAnimation(0, rand, pivotx, pivoty);
+            rot.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    spinning = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    spinning = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+            rot.setDuration(1000);
+            rot.setFillAfter(true);
+            lastde = rand;
+            bottle.startAnimation(rot);
+        }
+
+    }
+}
